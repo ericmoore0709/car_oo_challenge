24bab247ab661cb3c228545b6a4d53e443e65c82
Eric Moore <ericmoore0709@gmail.com>
2024-02-26 17:31:54 -0800

    Part 2

--- Car.js
+++ Car.js
@@ -1,32 +1,8 @@
-class Car {
+const Vehicle = require("./Vehicle");
 
-    /**
-     * Creates an instance of the Car class with a given make, model, and year.
-     * @param {string} make the make of the car
-     * @param {string} model the model of the car
-     * @param {string} year the year of the car 
-     */
-    constructor(make, model, year) {
-        this.make = make;
-        this.model = model;
-        this.year = year;
-    }
+class Car extends Vehicle {
 
-    /**
-     * Returns a `string` representation of the car.
-     * @returns a `string` representation of the car.
-     */
-    toString = () => {
-        return `The vehicle is a ${this.make} ${this.model} from ${this.year}.`;
-    };
-
-    /**
-     * Returns the sound of the car horn when honked. 
-     * @returns the sound of the car horn when honked.
-     */
-    honk = () => {
-        return "Beep.";
-    }
+    numWheels = 4;
 
 }
 
--- index.js
+++ index.js
@@ -2,3 +2,4 @@ const Car = require('./Car');
 const car1 = new Car("Toyota", "Camry", "2004");
 console.log(car1.honk());
 console.log(car1.toString());
+console.log(car1.numWheels);
